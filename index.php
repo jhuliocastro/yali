879eb5e9e7f4041fb6ecc9668f1115458987ce3f
jhuliocastro <jhuliocastro@hotmail.com>
2022-02-12 22:10:32 -0300

    CONFIGURACAO DE ROTAS

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require "vendor/autoload.php";
+
+use CoffeeCode\Router\Router;
+
+$router = new Router(URL);
+
+$router->namespace("Controller"); #DEFINICAO DO NAMESPACE
+
+//ROTAS
+$router->get("/", "Login:home");
+
+$router->group("error");
+$router->get("/{code}", "Error:home", "error.home");
+
+$router->dispatch();
+
+if($router->error()){
+    $router->redirect("error.name");
+}
