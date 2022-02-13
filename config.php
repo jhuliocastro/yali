dcc7851e780c9968221007aad72eb7d39f54ff32
jhuliocastro <jhuliocastro@hotmail.com>
2022-02-13 00:41:17 -0300

    ARQUIVO DE LOGIN

--- config.php
+++ config.php
@@ -1,2 +1,17 @@
 <?php
 const URL = "http://localhost";
+
+define("DATA_LAYER_CONFIG", [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "port" => "3306",
+    "dbname" => "yali",
+    "username" => "root",
+    "passwd" => "",
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
--- src/controllers/Login.php
+++ src/controllers/Login.php
@@ -1,12 +1,40 @@
 <?php
 namespace Controller;
 
+use Models\Login_Model;
+
 class Login extends Controller{
     public function home(){
         parent::render("login");
     }
 
     public function login(){
-        var_dump($_POST);
+        $dados = (object)$_POST;
+        $dados->senha = md5($dados->senha);
+
+        $retorno = null;
+        $model = new Login_Model();
+        var_dump($model);
+        $retornoEmail = $model->find()->fetch(true);
+        var_dump($model);
+        /* if($retornoEmail == 0){
+             $retornoUsuario = $model->retornoUsuario($dados->usuario);
+             if ($retornoUsuario > 0){
+                 $retorno = "usuario";
+             }
+         }else{
+             $retorno = "email";
+         }
+
+         if($retorno == "email"){
+             $dados = $model->loginEmail($dados->usuario, $dados->senha);
+         }else if($retorno == "usuario"){
+             $dados = $model->loginUsuario($dados->usuario, $dados->senha);
+         }
+
+         var_dump($dados);
+
+         session_start();
+         $_SESSION["usuario"] == $dados->id;*/
     }
 }
